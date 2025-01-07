dc40936c0991230c8e92bc099b26c022510603a1
MarcelineVQ <marcythevq@gmail.com>
2025-01-06 18:07:30 -0800

    [ fix ] force mouseover reset

--- RightClickModifier.lua
+++ RightClickModifier.lua
@@ -179,6 +179,7 @@ WorldFrame:SetScript("OnMouseUp", function (a1,a2,a3,a4,a5,a6,a7,a8,a9)
       end
     end
   end
+  RCM.mouseover_valid = false -- reset for next click
   if orig_onmouseup then orig_onmouseup(a1,a2,a3,a4,a5,a6,a7,a8,a9) end
 end)
 
